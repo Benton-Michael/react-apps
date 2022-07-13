8b65b45dbdcbad7cba9c60eb93a65b3ec1576472
Benton-Michael <mbenton002@gmail.com>
2022-07-13 17:51:54 -0600

    Completed Sidebar UI component for front-end

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -4,12 +4,12 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import { Ecommerce, Employees, Orders, Calendar, Stacked, Pyramid, Customers, 
-  Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages';
+  Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from './pages';
 
 import "./App.css";
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
