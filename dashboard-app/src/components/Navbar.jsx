382ccfd45dca9e569b9259022e47ec0eb8661b86
Benton-Michael <mbenton002@gmail.com>
2022-07-15 19:23:34 -0600

    Navbar with avatar image and cart, chat, and notification

--- dashboard-app/src/components/Navbar.jsx
+++ dashboard-app/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
-import { RiNotificationLine } from "react-icons/ri";
+import { RiNotification3Line } from "react-icons/ri";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import avatar from "../data/avatar.jpg";
+import mds from "../data/mds.jpg";
 import { Cart, Chat, Notification, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
@@ -14,35 +14,71 @@ import { useStateContext } from "../contexts/ContextProvider";
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
     {/* other NavButtons will do different actions - each NavButton calls the unique action we provide to it by using the customFunc  */}
-    <button 
-      type="button" 
-      onClick={customFunc} style= {{ color }} 
-      className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-      <span 
-        style={{ background: dotColor }} 
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-        >{icon}
-        </span>
+      >
+        {icon}
+      </span>
     </button>
-
   </TooltipComponent>
-)
+);
 
 const Navbar = () => {
-  
   const { activeMenu, setActiveMenu } = useStateContext();
-  
-  
-  
-  
-  return ( 
+
+  return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       {/* this code is unique to this nav button */}
-      <NavButton title="Menu" customFunc={() => 
-        setActiveMenu((prevActiveMenu) => 
-        !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("chat")}
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          customFunc={() => handleClick("notification")}
+          dotColor="rgb(254, 201, 15)"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img className="rounded-full w-8 h-8" src={mds} />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
     </div>
-)
+  );
 };
 
 export default Navbar;
