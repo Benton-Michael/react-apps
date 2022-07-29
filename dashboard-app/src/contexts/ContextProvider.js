d77bb90a90285b43eb5da2b82202636794dd0b03
Benton-Michael <mbenton002@gmail.com>
2022-07-29 15:27:10 -0600

    Added logic for the navbar

--- dashboard-app/src/contexts/ContextProvider.js
+++ dashboard-app/src/contexts/ContextProvider.js
@@ -13,12 +13,23 @@ export const ContextProvider = ({ children }) => {
 
     const [activeMenu, setActiveMenu] = useState (true);
 
+    const [isClicked, setIsClicked] = useState(initialState);
+
+    const handleClick = (clicked) => {
+        // open the object and spread the initial state
+        setIsClicked({ ...initialState, [clicked]:true });
+    }
+
     return (
     <StateContext.Provider
         value={{ 
             // test: 'test' 
             activeMenu,
             setActiveMenu,
+            isClicked,
+            setIsClicked,
+            handleClick,
+
     }}
     >
         { children }
